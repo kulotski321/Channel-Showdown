939e711cfb57d2dbea0a8020f382e9d3db57efc4
Angelo <angelocool177@gmail.com>
2018-03-01 23:54:07 +0800

    Added Register Screen

---
+++ app/src/main/java/com/example/cf/channelsd/RegisterActivity.kt
@@ -0,0 +1,16 @@
+package com.example.cf.channelsd
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Created by CF on 3/1/2018.
+ */
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
